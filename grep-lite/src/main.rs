1c72b4e6a66e028807f285f844bf8fb28a8e2529
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2021-12-22 13:57:27 +0900

    grep-lite: Get haystack by reading file

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let args = App::new("grep-lite")
@@ -11,20 +13,25 @@ fn main() {
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
+
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 
-    for line in quote.lines() {
-        let contains_substring = re.find(line);
-        match contains_substring {
-            // Some(T) は Option の肯定的なケース
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
             Some(_) => println!("{}", line),
-            // None は Option の否定的なケース
             None => (),
         }
     }
