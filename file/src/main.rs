b64fee0641c844edede8968cdec4d0b45445b388
Kazumasa Hirata <50480499+kzmsh@users.noreply.github.com>
2022-01-22 12:31:44 +0900

    ch3-not-quite-file-2

--- file/src/main.rs
+++ file/src/main.rs
@@ -14,20 +14,31 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-#[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    unimplemented!()
+fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+
+    read_length
 }
 
 fn main() {
-    let f1 = File {
-        name: String::from("f1.txt"),
-        data: Vec::new(),
+    let mut f1 = File {
+        name: String::from("f2.txt"),
+        data: vec![114, 117, 115, 116, 33],
     };
 
-    let f1_name = &f1.name;
-    let f1_length = &f1.data.len();
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f1);
+    let f1_length = read(&f1, &mut buffer);
+    close(&mut f1);
+
+    let text = String::from_utf8_lossy(&buffer);
 
     println!("{:?}", f1);
-    println!("{} is {} byte long", f1_name, f1_length);
+    println!("{} is {} byte long", &f1.name, f1_length);
+    println!("{}", text);
 }
